075fe42f154482e3158d2ee394840b9202ac3084
Daniel Rocha <dgustavo@id.uff.br>
2022-09-13 06:38:40 -0300

    test: ensure initial AdicionarProdutoController tests

---
+++ test/integration/AdicionarProdutoController.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const request = require('supertest')
+// const models = require('../../src/models/index')
+const app = require('../../app.js')
+
+describe('Integration Test AdicionarProdutoController', function () {
+//   beforeEach(() => {
+//     models.Category.destroy({ where: {} })
+//     models.Product.destroy({ where: {} })
+//   })
+//   afterAll(() => {
+//     models.Category.destroy({ where: {} })
+//     models.Product.destroy({ where: {} })
+//     models.sequelize.close()
+//   })
+//   afterEach(() => {
+//     models.Category.destroy({ where: {} })
+//     models.Product.destroy({ where: {} })
+//   })
+  // TODO: make the test after a route to add category
+  // TODO:add a new test to delete product with auth
+  //   test('should give a 200 when product was found', async () => {
+  //     // Arrange
+  //     await request(app)
+  //       .post('/adicionarProduto')
+  //       .send({ nome: 'Cassete 9V 11-36D CS-HG201 - Shimano', category: '1', description: 'Cassete para bicicleta, marca Shimano, modelo CS-HG201, 9 velocidades, 11 x 36 dentes na cor prata /cromado.', SKU: '11111', price: '230', image: '/assets/products/003-cassete.jpg' })
+  //     // Act
+  //     const res = await request(app).get('/produto/1')
+  //     // Assert
+  //     expect(res.header['content-type']).toBe('text/html; charset=utf-8')
+  //     expect(res.statusCode).toBe(200)
+  //   })
+
+  test('should give a 302 redirect when access / without auth', async () => {
+    // Arrange
+    // Act
+    const res = await request(app).get('/adicionarProduto')
+    // Assert
+    expect(res.statusCode).toBe(302)
+  })
+})
