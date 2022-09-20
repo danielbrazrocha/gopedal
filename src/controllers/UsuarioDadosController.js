fcdef924aecbda85e60586443a77cb1d51d53a1c
Daniel Rocha <dgustavo@id.uff.br>
2022-09-19 21:01:44 -0300

    feat: add user details page

---
+++ src/controllers/UsuarioDadosController.js
@@ -0,0 +1,104 @@
+const { User } = require('../models')
+const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+
+const UsuarioDadosController = {
+
+  show: async (req, res) => {
+    const { id } = req.session.user
+    try {
+      const userDetails = await User.findOne({
+        where: {
+          id
+        },
+        include: ['addresses']
+      })
+
+      if (userDetails.length === 0) {
+        return res.status(200).render('usuario', {
+          arquivoCss: 'dashboard.css',
+          error: 'Não há nenhuma informação cadastrada.'
+        })
+      }
+
+      return res.status(200).render('usuario', {
+        arquivoCss: 'dashboard.css',
+        dados: userDetails
+      })
+    } catch (error) {
+      return res.status(500).json({ message: 'Error' + error })
+    }
+  },
+  submitEdit: async (req, res) => {
+    console.log(req)
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+      const { id, nome, cpf, tel, email, senha, repeteSenha } = req.body
+      let passCrypt
+      if (senha && senha === repeteSenha) {
+        passCrypt = bcrypt.hashSync(senha, 10)
+      }
+      try {
+        const newItemData = {
+          name: nome,
+          cpf,
+          tel,
+          email,
+          senha: passCrypt,
+          updatedAt: new Date().toISOString()
+        }
+
+        const ans = await User.update(newItemData, {
+          where: {
+            id
+          }
+        })
+
+        if (!ans) {
+          return res.status(422).render('usuario', {
+            arquivoCss: 'dashboard.css',
+            error: `Erro na atualização do usuário ${id}. Verifique as informações e tente novamente.`
+          })
+        }
+        return res.status(201).render('usuario', {
+          arquivoCss: 'dashboard.css',
+          success: `Usuário Id ${id} atualizado com sucesso.`
+        })
+      } catch (err) {
+        return res.status(500).render('usuario', {
+          arquivoCss: 'dashboard.css',
+          error: 'Erro interno no sistema. Entre em contato com o administrador.'
+        })
+      }
+    } else {
+      return res.status(422).render('usuario', {
+        arquivoCss: 'dashboard.css',
+        errors: errors.errors
+      })
+    }
+  },
+  delete: async (req, res) => {
+    const id = req.params.id
+    await User.destroy({
+      where: {
+        id
+      }
+    })
+      .then(function (deletedRecord) {
+        if (deletedRecord === 1) {
+          return res.redirect('/login/logout')
+        } else {
+          return res.status(422).render('usuario', {
+            arquivoCss: 'dashboard.css',
+            error: `Erro na exclusão do usuário ${id}. Procure o suporte.`
+          })
+        }
+      })
+      .catch(function (error) {
+        res.status(500).json(error)
+      })
+  }
+}
+
+module.exports = UsuarioDadosController
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const UsuarioController = require('../controllers/UsuarioController')
+const UsuarioDadosController = require('../controllers/UsuarioDadosController')
+const userValidator = require('../middlewares/validator')
 const isAuth = require('../middlewares/isAuth')
 
 router.get('/', isAuth, UsuarioController.index)
+
+router.get('/dados', isAuth, UsuarioDadosController.show)
+router.post('/dados/:id', isAuth, userValidator, UsuarioDadosController.submitEdit)
+router.get('/dados/deletar/:id', isAuth, UsuarioDadosController.delete)
 
 module.exports = router
