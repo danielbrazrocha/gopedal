c4b1362764e88f7862d8021adf6b5945d575fd2b
Daniel Rocha <dgustavo@id.uff.br>
2022-09-29 17:35:06 -0300

    test: ensure ProdutoController full test coverage

--- test/integration/ProdutoController.test.js
+++ test/integration/ProdutoController.test.js
@@ -3,33 +3,43 @@ const request = require('supertest')
 const models = require('../../src/models/index')
 const app = require('../../app.js')
 
+// Factory to make Product with an Id
+const makeProduct = (id) => {
+  return models.Product.create({
+    // CategoryId: 1,
+    // DiscountId: 1,
+    id,
+    InventoryId: id,
+    name: 'Pedal Clip MTB PD-M324 (Prata) - Shimano',
+    description: 'Pedal clip para bicicleta MTB, marca Shimano, modelo PD-M324, cor Prata. Especificação:- Marca: Shimano - Modelo: PD-M324- Código Shimano: EPDM324 - Cor: Prata- Rosca: Grossa tipo Inglês 9/16"- Acompanha par de taquinho: SM-SH56- Sistema misto, sendo um lado com clip e o outro com apoio normal (sem clip)- Peso: 533 gramas (par)- Produto original.',
+    image: '/assets/products/001-PedalClip.jpg',
+    SKU: '1111',
+    price: 520.00,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+}
+
+// Factory to make Inventory with an Id
+const makeInventory = (id) => {
+  return models.Inventory.create({
+    id,
+    quantity: 100,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+}
+
 describe('Integration Test SobreController', function () {
   beforeEach(() => {
-    models.Category.destroy({ where: {} })
     models.Product.destroy({ where: {} })
   })
   afterAll(() => {
-    models.Category.destroy({ where: {} })
-    models.Product.destroy({ where: {} })
     models.sequelize.close()
   })
   afterEach(() => {
-    models.Category.destroy({ where: {} })
     models.Product.destroy({ where: {} })
   })
-  // TODO: make the test after a route to add category
-  // TODO:add a new test to delete product with auth
-  //   test('should give a 200 when product was found', async () => {
-  //     // Arrange
-  //     await request(app)
-  //       .post('/adicionarProduto')
-  //       .send({ nome: 'Cassete 9V 11-36D CS-HG201 - Shimano', category: '1', description: 'Cassete para bicicleta, marca Shimano, modelo CS-HG201, 9 velocidades, 11 x 36 dentes na cor prata /cromado.', SKU: '11111', price: '230', image: '/assets/products/003-cassete.jpg' })
-  //     // Act
-  //     const res = await request(app).get('/produto/1')
-  //     // Assert
-  //     expect(res.header['content-type']).toBe('text/html; charset=utf-8')
-  //     expect(res.statusCode).toBe(200)
-  //   })
 
   test('should give a 404 when product was not found', async () => {
     // Arrange
@@ -38,5 +48,15 @@ describe('Integration Test SobreController', function () {
     // Assert
     expect(res.header['content-type']).toBe('text/html; charset=utf-8')
     expect(res.statusCode).toBe(404)
+  })
+
+  test('should receive a 200 when product was found', async () => {
+    // Arrange
+    makeInventory(20)
+    makeProduct(20)
+    // Act
+    const res = await request(app).get('/produto/20')
+    // Assert
+    expect(res.status).toBe(200)
   })
 })
